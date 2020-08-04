06deefe3784ce2105bc1dbce49a99857e7d2b7e2
Abryan Manalansang <42795149+Yamongski@users.noreply.github.com>
2020-08-05 07:24:14 +0800

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd /var/www/vhosts/yan.bbtdevelopment.com/httpdocs/
 sudo npm install
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
